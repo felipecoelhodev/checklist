282eb9d360b1962d00531f903e8ca3eb670ddc65
felipecoelhodev <felipecoelhoflorenciotrab@gmail.com>
2026-03-05 09:07:29 -0300

    finalizando o crud

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { use, useState } from "react";
+import { use } from "react";
 import { ChecklistsWrapper } from "./components/ChecklistsWrapper";
 import { Container } from "./components/Container";
 import { Dialog } from "./components/Dialog";
@@ -10,18 +10,26 @@ import { IconPlus, IconSchool } from "./components/icons";
 import { TodoForm } from "./components/TodoForm";
 import TodoContext from "./components/TodoProvider/TodoContext";
 import { TodoGroup } from "./components/TodoGroup";
+import { EmptyState } from "./components/EmptyState";
 
 function App() {
-  const [showDialog, setShowDialog] = useState(false);
-  const { todos, addTodo } = use(TodoContext);
-
-  const toggleDialog = () => {
-    setShowDialog(!showDialog);
-  };
+  const {
+    todos,
+    addTodo,
+    showDialog,
+    openFormTodoDialog,
+    closeFormTodoDialog,
+    selectedTodo,
+    editTodo,
+  } = use(TodoContext);
 
   const handleFormSubmit = (formData) => {
-    addTodo(formData);
-    toggleDialog();
+    if (selectedTodo) {
+      editTodo(formData);
+    } else {
+      addTodo(formData);
+    }
+    closeFormTodoDialog();
   };
 
   return (
@@ -37,15 +45,19 @@ function App() {
             heading="Para estudar"
             items={todos.filter((t) => !t.completed)}
           />
+          {todos.length == 0 && <EmptyState />}
           <TodoGroup
             heading="Concluído"
             items={todos.filter((t) => t.completed)}
           />
           <Footer>
-            <Dialog isOpen={showDialog} onClose={toggleDialog}>
-              <TodoForm onSubmit={handleFormSubmit} />
+            <Dialog isOpen={showDialog} onClose={closeFormTodoDialog}>
+              <TodoForm
+                onSubmit={handleFormSubmit}
+                defaultValue={selectedTodo?.description}
+              />
             </Dialog>
-            <FabButton onClick={toggleDialog}>
+            <FabButton onClick={() => openFormTodoDialog()}>
               <IconPlus />
             </FabButton>
           </Footer>
--- src/components/TodoProvider/index.jsx
+++ src/components/TodoProvider/index.jsx
@@ -7,6 +7,20 @@ export function TodoProvider({ children }) {
   const savedTodos = localStorage.getItem(TODOS);
 
   const [todos, setTodos] = useState(savedTodos ? JSON.parse(savedTodos) : []);
+  const [showDialog, setShowDialog] = useState(false);
+  const [selectedTodo, setSelectedTodo] = useState();
+
+  const openFormTodoDialog = (todo) => {
+    if (todo) {
+      setSelectedTodo(todo);
+    }
+    setShowDialog(true);
+  };
+
+  const closeFormTodoDialog = () => {
+    setShowDialog(false);
+    setSelectedTodo(null);
+  };
 
   useEffect(() => {
     localStorage.setItem(TODOS, JSON.stringify(todos));
@@ -39,6 +53,20 @@ export function TodoProvider({ children }) {
     });
   };
 
+  const editTodo = (formData) => {
+    setTodos((prevState) => {
+      return prevState.map((t) => {
+        if (t.id == selectedTodo.id) {
+          return {
+            ...t,
+            description: formData.get("description"),
+          };
+        }
+        return t;
+      });
+    });
+  };
+
   const deleteTodo = (todo) => {
     setTodos((prevState) => {
       return prevState.filter((t) => t.id != todo.id);
@@ -51,6 +79,11 @@ export function TodoProvider({ children }) {
         addTodo,
         toggleTodoCompleted,
         deleteTodo,
+        showDialog,
+        openFormTodoDialog,
+        closeFormTodoDialog,
+        selectedTodo,
+        editTodo,
       }}
     >
       {children}
